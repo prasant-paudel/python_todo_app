a1d0158f0354397703ffd567b5be5affeebe1800
Prasant Paudel <prashant.paudel.555@gmail.com>
2020-05-14 10:02:35 +0545

    deletion also added

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def update():
     # print(request.form['1'])
 
     # detecting max id
-    r = db.engine.execute('select max(id) from todo')
+    r = db.engine.execute('SELECT MAX(id) FROM todo')
     global id_max
     id_max = 0
     for x in r:
@@ -43,28 +43,25 @@ def update():
         try:
             value = request.form[f'{id}']
             if str(value) == 'on':
-                print('Status of', id, end=': ')
-                print(Todo.query.all()[id-1].complete)
-                
-                db.engine.execute(f'update todo set complete=1 where id={id};')
-                r = db.engine.execute(f'select * from todo where id={id};')
-                for i in r:
-                    print(i)
-                
-                print('Status of', id, end=': ')
-                print(Todo.query.all()[id-1].complete)
-                
-
-                
+                db.engine.execute(f'UPDATE todo SET complete=1 WHERE id={id};')
         except:
-            db.engine.execute(f'update todo set complete=0 where id={id};')
-            # db.engine.execute(f'update todo set complete=0 where id=1;')
-            print(id, id_max)
-            continue
-
-
-
+            db.engine.execute(f'UPDATE todo SET complete=0 WHERE id={id};')
     return redirect(url_for('index'))
+
+
+@app.route('/delete')
+def delete():
+    id = request.args['id']
+    db.engine.execute(f"DELETE FROM todo WHERE id={id}")
+    return redirect(url_for('index'))
+
+
+# query to the table todo
+r = db.engine.execute(f'SELECT * FROM todo;')
+print('')
+for i in r:
+    print(i)
+print('')
 
 if __name__ == '__main__':
     app.run()
